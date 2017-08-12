126a835bb9c3e6984a10406701a88f1c027b0590
avkiranmayi <30668930+avkiranmayi@users.noreply.github.com>
2017-08-12 22:56:54 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,6 @@ button.onclick = function () {
         }
     };
     //Make the request
-    request.open('GET', 'http://avkiranmayi.imad.hasura.io/counter', true);
+    request.open('GET', 'http://avkiranmayi.imad.hasura-app.io/counter', true);
     request.send(null);
    }
